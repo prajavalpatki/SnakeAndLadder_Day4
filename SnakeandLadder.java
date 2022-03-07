bade501fd6a56963d30973f0c1bf2198cee71bea
prajavalpatki <prajavalpatki.com>
2022-03-08 02:29:10 +0530

    They are no play Ladder or Snake

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -3,13 +3,13 @@ package com.bridgelabz;
 public class SnakeandLadder {
 
     public static void main(String[] args) {
-
         int position = 0;
-
-        int randomCheck = (int) Math.floor (Math.random() * 10) % 6;
-
-        position = position + randomCheck;
-
+        int play = 1;
+        int playornot = (int) Math.floor (Math.random() * 10) %2;
+        if (playornot == play) {
+            int randomCheck = (int) Math.floor(Math.random() * 10) % 6;
+            position = position + randomCheck;
+        }
         System.out.println("position = " + position);
     }
 }

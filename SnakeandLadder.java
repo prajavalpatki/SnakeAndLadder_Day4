57937ec4484743a6843b046ecd87956bf8b96853
prajavalpatki <prajavalpatki.com>
2022-03-08 02:19:27 +0530

    Single player at started position 0

---
+++ SnakeandLadder.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class SnakeandLadder {
+
+    public static void main(String[] args) {
+
+        int position = 0;
+
+        System.out.println("position =" + position);
+    }
+}
